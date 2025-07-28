3c4c6ef7fe0920c546769d80962a0ea0dee81a59
momen124 <momenhossny2004@gmail.com>
2025-07-28 18:07:27 +0300

    feat/API: Finalize Phase 3 API endpoints for Movie model

--- movie/movieapp/urls.py
+++ movie/movieapp/urls.py
@@ -1,5 +1,10 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import MovieViewSet
+
+router = DefaultRouter()
+router.register(r'movies', MovieViewSet)
 
 urlpatterns = [
-      # Add view routes in later phases
+      path('', include(router.urls)),
   ]
