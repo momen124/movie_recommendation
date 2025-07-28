5660db84853bab6ebeabc18dc5c3520271b7c3c0
momen124 <momenhossny2004@gmail.com>
2025-07-28 18:15:12 +0300

    feat/Authentication: Add token and refresh endpoints to urls

--- movie/movieapp/urls.py
+++ movie/movieapp/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import MovieViewSet
+from .views import MovieViewSet, token_obtain_pair, token_refresh
 
 router = DefaultRouter()
 router.register(r'movies', MovieViewSet)
 
 urlpatterns = [
       path('', include(router.urls)),
+       path('token/', token_obtain_pair, name='token_obtain_pair'),
+      path('token/refresh/', token_refresh, name='token_refresh'),
   ]
