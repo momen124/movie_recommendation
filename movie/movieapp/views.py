e648c02641dd1538c0037a22fdaf542ce2ef3bac
momen124 <momenhossny2004@gmail.com>
2025-07-28 18:13:55 +0300

    feat/Authentication: Add token obtain and refresh views

--- movie/movieapp/views.py
+++ movie/movieapp/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .models import Movie
 from .serializers import MovieSerializer
 
@@ -7,3 +8,7 @@ class MovieViewSet(viewsets.ModelViewSet):
       queryset = Movie.objects.all()
       serializer_class = MovieSerializer
       permission_classes = [IsAuthenticatedOrReadOnly]
+
+  # Add authentication views
+token_obtain_pair = TokenObtainPairView.as_view()
+token_refresh = TokenRefreshView.as_view()
